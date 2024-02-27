92f741d9f101321d27bbe50e35e08b3230fa0818
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-02-27 11:59:14 +0100

    Completed Generate Characters

---
+++ 3. Fundamentals/Lists/GenerateCharacters/GenerateCharacters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenerateCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> abilityScores = new List<int>();
+
+            Random random = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                List<int> temp = new List<int>();
+                for (int j = 0; j < 4; j++)
+                {
+                    temp.Add(random.Next(1, 7));
+                }
+                string tempScores = String.Join(", ", temp);
+                Console.Write($"You roll {tempScores}. ");
+                temp.Remove(temp.Min());
+                int totalTemp = temp.Sum();
+                Console.Write($"The ability score is {totalTemp}\n");
+                abilityScores.Add(totalTemp);
+            }
+            abilityScores.Sort();
+            string scoresText = String.Join(", ", abilityScores);
+
+            Console.WriteLine($"Your available ability scores are {scoresText}");
+
+            // to keep console open
+            Console.ReadLine();
+        }
+    }
+}
