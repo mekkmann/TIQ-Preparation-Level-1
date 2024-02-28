0fef3c1b5893b06dd055cc33313ec3e5006d0f14
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-02-28 09:40:23 +0100

    IncreasingLevelDifficulty Completed

---
+++ 3. Fundamentals/Data Structures/Arrays/IncreasingLevelDifficulty/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IncreasingLevelDifficulty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[] levels = new int[100];
+
+            for (int i = 0; i < levels.Length; i++)
+            {
+                levels[i] = random.Next(1, 51);
+            }
+
+            Array.Sort(levels);
+
+            Console.WriteLine(
+                $"Number of monsters in levels: {string.Join(", ", levels)}"
+                );
+
+            // to keep console open
+            Console.ReadLine();
+        }
+    }
+}
