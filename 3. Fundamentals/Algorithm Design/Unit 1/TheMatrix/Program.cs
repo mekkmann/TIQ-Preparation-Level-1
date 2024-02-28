8e02806cf1e669b4b3fbe353ecbd809e5ef3688b
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-02-28 15:07:03 +0100

    TheMatrix Completed

---
+++ 3. Fundamentals/Algorithm Design/Unit 1/TheMatrix/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TheMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new();
+            var streams = new List<int>();
+            var symbols = "!@#$%^&*()_+-=[];',.\\/~{}:|<>?";
+
+            for (var i = 0; i < 10; i++) streams.Add(random.Next(0, 80));
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            while (true)
+            {
+                for (int x = 0; x < 80; x++)
+                {
+                    Console.Write(streams.Contains(x) ? symbols[random.Next(symbols.Length)] : ' ');
+                }
+
+                Console.WriteLine();
+                Thread.Sleep(100);
+
+                if (random.Next(3) == 0) streams.Add(random.Next(0, 80));
+                if (random.Next(3) == 0) streams.RemoveAt(random.Next(streams.Count));
+            }
+        }
+    }
+}
