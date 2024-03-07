f576248a2512ea41cf14646dff8d0c56a2b5d3c4
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-07 22:55:50 +0100

    mission 1 unit 1 comments

--- 4. Preparation/Dice Simulator/DiceSimulator/Program.cs
+++ 4. Preparation/Dice Simulator/DiceSimulator/Program.cs
@@ -4,35 +4,40 @@
     {
         static void Main(string[] args)
         {
-            int[] smallNote = [
-                                87,  97, 110, 116,  32, 116, 111,  32, 109, 101,
-                                101, 116,  32, 102, 111, 114,  32, 108, 117, 110,
-                                 99, 104,  63,  32,  73,  39, 108, 108,  32, 108,
-                                101,  97, 118, 101,  32, 116, 104, 101,  32, 114,
-                                101, 115, 116,  97, 117, 114,  97, 110, 116,  32,
-                                 97, 100, 100, 114, 101, 115, 115,  32, 105, 110,
-                                 32, 116, 104, 101,  32, 115, 111, 117, 116, 104,
-                                 32, 109,  97, 105, 110, 116, 101, 110,  97, 110,
-                                 99, 101,  32,  99, 108, 111, 115, 101, 116,  46,
-                                 32,  66, 114, 105, 110, 103,  32,  97, 110,  32,
-                                 65,  83,  67,  73,  73,  32,  99, 104,  97, 114,
-                                116,  44,  32, 116, 104, 101,  32, 109, 101, 115,
-                                115,  97, 103, 101,  32, 119, 105, 108, 108,  32,
-                                 98, 101,  32,  99, 111, 100, 101, 100,  46
-                            ];
+            // START MISSION 1
+            // initiate an array of ints with the numbers on the small note
+            //int[] smallNote = [
+            //                    87,  97, 110, 116,  32, 116, 111,  32, 109, 101,
+            //                    101, 116,  32, 102, 111, 114,  32, 108, 117, 110,
+            //                     99, 104,  63,  32,  73,  39, 108, 108,  32, 108,
+            //                    101,  97, 118, 101,  32, 116, 104, 101,  32, 114,
+            //                    101, 115, 116,  97, 117, 114,  97, 110, 116,  32,
+            //                     97, 100, 100, 114, 101, 115, 115,  32, 105, 110,
+            //                     32, 116, 104, 101,  32, 115, 111, 117, 116, 104,
+            //                     32, 109,  97, 105, 110, 116, 101, 110,  97, 110,
+            //                     99, 101,  32,  99, 108, 111, 115, 101, 116,  46,
+            //                     32,  66, 114, 105, 110, 103,  32,  97, 110,  32,
+            //                     65,  83,  67,  73,  73,  32,  99, 104,  97, 114,
+            //                    116,  44,  32, 116, 104, 101,  32, 109, 101, 115,
+            //                    115,  97, 103, 101,  32, 119, 105, 108, 108,  32,
+            //                     98, 101,  32,  99, 111, 100, 101, 100,  46
+            //                ];
 
-            string decipheredNote = string.Empty;
+            // initiate an empty string to store the converted values
+            //string decipheredNote = string.Empty;
 
-            foreach (int value in smallNote)
-            {
-                decipheredNote += Convert.ToChar(value);
-            }
+            /* going through every value in the smallNote array,
+             * we add the converted value to the decipheredNote string */
+            //foreach (int value in smallNote)
+            //{
+            //    decipheredNote += Convert.ToChar(value);
+            //}
 
-            Console.WriteLine(decipheredNote);
-
+            // write the decipheredNote to the console
+            //Console.WriteLine(decipheredNote);
             //to keep console open
-            Console.ReadLine();
-
+            //Console.ReadLine();
+            // END MISSION 1
         }
     }
 }
