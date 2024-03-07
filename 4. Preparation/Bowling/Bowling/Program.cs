b9f0086634540fbb348e85408fb50f0029d33fcf
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-07 23:34:22 +0100

    start Mission 1 Unit 1

---
+++ 4. Preparation/Bowling/Bowling/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Bowling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //START MISSION 1
+
+            //END MISSION 1
+        }
+
+    }
+}
