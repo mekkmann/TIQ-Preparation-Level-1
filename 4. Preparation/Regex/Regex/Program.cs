35ae839768bc622828450096b3277b2cc51fe1fb
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-08 09:10:19 +0100

    Regex Unit 1 Mission 1 Completed

---
+++ 4. Preparation/Regex/Regex/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // initiate a string with the path to a file
+            string pathToFile = "MonsterManual.txt";
+
+            // read all lines of a file and store them in an array of string
+            string[] readFile = File.ReadAllLines(pathToFile);
+
+            for (int i = 0; i < readFile.Length - 1; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(readFile[i]);
+                }
+            }
+
+            //to keep console open
+            Console.ReadLine();
+        }
+    }
+}
