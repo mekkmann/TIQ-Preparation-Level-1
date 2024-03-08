8eaf10559f48dedfecb363da45b6d82d50da5fc7
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-08 13:36:43 +0100

    Update Program.cs

--- 4. Preparation/Regex/Regex/Program.cs
+++ 4. Preparation/Regex/Regex/Program.cs
@@ -26,6 +26,32 @@
             // a list of strings to store names and specific alignment of monsters
             List<string> namesAndSpecificAlignment = [];
 
+            // array of string to keep to be able to index alignment to names in namesByAlignment
+            string[] firstAlignment = ["lawful", "neutral", "chaotic"];
+            // array of string to keep to be able to index alignment to names in namesByAlignment
+            string[] secondAlignment = ["good", "neutral", "evil"];
+
+            // 2d array of lists to track specific alignment of monsters
+            // -- index will correspond to the firstAlignment and SecondAlignment above ^
+            List<string>[,] namesByAlignment = new List<string>[3, 3];
+            // populating the namesByAlignment array with empty lists
+            for (int axis1 = 0; axis1 < 3; axis1++)
+            {
+                for (int axis2 = 0; axis2 < 3; axis2++)
+                {
+                    namesByAlignment[axis1, axis2] = new List<string>();
+                }
+            }
+
+            // list of strings to keep track of unaligned monsters
+            List<string> namesOfUnaligned = [];
+            // list of strings to keep track of monsters with any alignment
+            List<string> namesOfAnyAlignment = [];
+            // list of strings to keep track of special case alignment monsters
+            List<string> namesOfSpecialCases = [];
+            // to keep track of the special case alignments
+            List<string> specialCases = [];
+
             // counter to keep track of if we're in a specific monsters block or are at the next
             int monsterLineCounter = 0;
 
@@ -35,8 +61,11 @@
             // goes through the read file line by line
             foreach (var line in readFile)
             {
+                // increment to the next line of the same monster
+                monsterLineCounter++;
+
                 //name
-                if (monsterLineCounter == 0)
+                if (monsterLineCounter == 1)
                 {
                     // add name to nameList
                     nameList.Add(line);
@@ -45,19 +74,45 @@
                 }
 
                 //alignment
-                if (monsterLineCounter == 1)
+                if (monsterLineCounter == 2)
                 {
                     string splitLine = line.Split(", ")[1];
-                    Match match = Regex.Match(splitLine, @"(chaotic|lawful|neutral) ?(good|neutral|evil)?");
+                    Match match = Regex.Match(splitLine, @"(chaotic|lawful|neutral) (good|neutral|evil)");
 
                     if (match.Success)
                     {
                         namesAndSpecificAlignment.Add($"{currentName} ({splitLine})");
+                        int firstAlignmentIndex = Array.IndexOf(firstAlignment, match.Groups[1].Value);
+                        int secondAlignmentIndex = Array.IndexOf(secondAlignment, match.Groups[2].Value);
+                        namesByAlignment[firstAlignmentIndex, secondAlignmentIndex].Add(currentName);
+                        continue;
                     }
+
+                    if (line.Contains("neutral"))
+                    {
+                        namesByAlignment[1, 1].Add(currentName);
+                        continue;
+                    }
+
+                    if (line.Contains("unaligned"))
+                    {
+                        namesOfUnaligned.Add(currentName);
+                        continue;
+                    }
+
+                    if (line.Contains("any alignment"))
+                    {
+                        namesOfAnyAlignment.Add(currentName);
+                        continue;
+                    }
+
+                    namesOfSpecialCases.Add(currentName);
+                    string[] parts = line.Split(", ");
+                    specialCases.Add(parts[1]);
                 }
 
                 //hitpoints
-                if (monsterLineCounter == 2)
+                if (monsterLineCounter == 3)
                 {
                     // if the monster takes 10+ rolls for Hp, add to tenPlusDiceRollsForHP
                     tenPlusDiceRollsForHP.Add(Regex.IsMatch(line, @"\(\d{2}d"));
@@ -65,7 +120,7 @@
                 }
 
                 //flying
-                if (monsterLineCounter == 4)
+                if (monsterLineCounter == 5)
                 {
                     // if the monster can fly, add to canFly
                     canFly.Add(line.Contains("fly"));
@@ -83,12 +138,8 @@
                     // if line is null or whitespace, we're at a new monster
                     monsterLineCounter = 0;
                 }
-                else
-                {
-                    // anything else we increment to the next line of the same monster
-                    monsterLineCounter++;
-                }
             }
+
             // print monster name and ability to fly to console
             Console.WriteLine("Monsters in the manual are:");
             for (var i = 0; i < nameList.Count; i++)
@@ -115,6 +166,46 @@
             foreach (var monster in namesAndSpecificAlignment)
             {
                 Console.WriteLine(monster);
+            }
+
+            // prints all specific alignments and the monster with those alignments
+            for (int secondAlignmentIndex = 0; secondAlignmentIndex < 3; secondAlignmentIndex++)
+            {
+                for (int firstAlignmentIndex = 0; firstAlignmentIndex < 3; firstAlignmentIndex++)
+                {
+                    if (firstAlignmentIndex == 1 && secondAlignmentIndex == 1)
+                    {
+                        Console.WriteLine($"\nMonsters with alignment true neutral are:");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nMonsters with alignment {firstAlignment[firstAlignmentIndex]} {secondAlignment[secondAlignmentIndex]} are:");
+                    }
+
+                    foreach (string name in namesByAlignment[firstAlignmentIndex, secondAlignmentIndex])
+                    {
+                        Console.WriteLine(name);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            // prints unaligned monster
+            Console.WriteLine($"\nUnaligned monsters are:");
+            foreach (string name in namesOfUnaligned)
+            {
+                Console.WriteLine(name);
+            }
+            // prints monsters of any alignment
+            Console.WriteLine($"\nMonsters which can be of any alignment are:");
+            foreach (string name in namesOfAnyAlignment)
+            {
+                Console.WriteLine(name);
+            }
+            // prints monster with special case alignments
+            Console.WriteLine($"\nMonsters with special cases are:");
+            for (int i = 0; i < namesOfSpecialCases.Count; i++)
+            {
+                Console.WriteLine($"{namesOfSpecialCases[i]} ({specialCases[i]})");
             }
 
             //to keep console open
