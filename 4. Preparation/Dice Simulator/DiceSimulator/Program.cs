806d5c67965611b3a237271e9a06fbcd72baa11f
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-08 17:56:33 +0100

    DiceSim Unit 1 Mission 2 completed

--- 4. Preparation/Dice Simulator/DiceSimulator/Program.cs
+++ 4. Preparation/Dice Simulator/DiceSimulator/Program.cs
@@ -4,40 +4,51 @@
     {
         static void Main(string[] args)
         {
-            // START MISSION 1
             // initiate an array of ints with the numbers on the small note
-            //int[] smallNote = [
-            //                    87,  97, 110, 116,  32, 116, 111,  32, 109, 101,
-            //                    101, 116,  32, 102, 111, 114,  32, 108, 117, 110,
-            //                     99, 104,  63,  32,  73,  39, 108, 108,  32, 108,
-            //                    101,  97, 118, 101,  32, 116, 104, 101,  32, 114,
-            //                    101, 115, 116,  97, 117, 114,  97, 110, 116,  32,
-            //                     97, 100, 100, 114, 101, 115, 115,  32, 105, 110,
-            //                     32, 116, 104, 101,  32, 115, 111, 117, 116, 104,
-            //                     32, 109,  97, 105, 110, 116, 101, 110,  97, 110,
-            //                     99, 101,  32,  99, 108, 111, 115, 101, 116,  46,
-            //                     32,  66, 114, 105, 110, 103,  32,  97, 110,  32,
-            //                     65,  83,  67,  73,  73,  32,  99, 104,  97, 114,
-            //                    116,  44,  32, 116, 104, 101,  32, 109, 101, 115,
-            //                    115,  97, 103, 101,  32, 119, 105, 108, 108,  32,
-            //                     98, 101,  32,  99, 111, 100, 101, 100,  46
-            //                ];
+            int[] smallNote = [
+                                87,  97, 110, 116,  32, 116, 111,  32, 109, 101,
+                                101, 116,  32, 102, 111, 114,  32, 108, 117, 110,
+                                 99, 104,  63,  32,  73,  39, 108, 108,  32, 108,
+                                101,  97, 118, 101,  32, 116, 104, 101,  32, 114,
+                                101, 115, 116,  97, 117, 114,  97, 110, 116,  32,
+                                 97, 100, 100, 114, 101, 115, 115,  32, 105, 110,
+                                 32, 116, 104, 101,  32, 115, 111, 117, 116, 104,
+                                 32, 109,  97, 105, 110, 116, 101, 110,  97, 110,
+                                 99, 101,  32,  99, 108, 111, 115, 101, 116,  46,
+                                 32,  66, 114, 105, 110, 103,  32,  97, 110,  32,
+                                 65,  83,  67,  73,  73,  32,  99, 104,  97, 114,
+                                116,  44,  32, 116, 104, 101,  32, 109, 101, 115,
+                                115,  97, 103, 101,  32, 119, 105, 108, 108,  32,
+                                 98, 101,  32,  99, 111, 100, 101, 100,  46
+                            ];
 
             // initiate an empty string to store the converted values
-            //string decipheredNote = string.Empty;
+            string decipheredNote = string.Empty;
 
             /* going through every value in the smallNote array,
              * we add the converted value to the decipheredNote string */
-            //foreach (int value in smallNote)
-            //{
-            //    decipheredNote += Convert.ToChar(value);
-            //}
+            foreach (int value in smallNote)
+            {
+                decipheredNote += Convert.ToChar(value);
+            }
 
             // write the decipheredNote to the console
-            //Console.WriteLine(decipheredNote);
+            Console.WriteLine(decipheredNote);
+
+            // initiate an array with size 256
+            int[] allNumbers = new int[256];
+            // populate array with numbers 0-255
+            for (int i = 0; i < 256; i++)
+            {
+                allNumbers[i] = i;
+            }
+
+            foreach (int number in allNumbers)
+            {
+                Console.WriteLine($"{number} = {Convert.ToChar(number)}");
+            }
             //to keep console open
-            //Console.ReadLine();
-            // END MISSION 1
+            Console.ReadLine();
         }
     }
 }
