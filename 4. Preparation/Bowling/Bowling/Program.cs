a40d5d2b8bc1f68bbe4e688b3087ed7e96ac8e1a
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-09 22:39:09 +0100

    Bowling Unit1 Mission1 Completed

--- 4. Preparation/Bowling/Bowling/Program.cs
+++ 4. Preparation/Bowling/Bowling/Program.cs
@@ -4,9 +4,46 @@
     {
         static void Main(string[] args)
         {
-            //START MISSION 1
+            // initiate a new random
+            Random random = new Random();
+            // set totalFrames to a random number between 1-10
+            int totalFrames = random.Next(1, 11);
 
-            //END MISSION 1
+            DrawEmptyFrames(totalFrames);
+            totalFrames = random.Next(1, 11);
+            DrawEmptyFrames(totalFrames);
+
+            Console.ReadLine();
+        }
+
+        static void DrawEmptyFrames(int frames)
+        {
+            //path to emptyFrame ascii art
+            string pathToEmptyFrame = "EmptyFrame.txt";
+            //path to closingFrame ascii art
+            string pathToClosingFrame = "FrameClosing.txt";
+            // read file and store all lines in an array
+            string[] emptyFrameLines = File.ReadAllLines(pathToEmptyFrame);
+            string[] closingFrameLines = File.ReadAllLines(pathToClosingFrame);
+
+            //for each line in emptyFrameLines
+            for (var i = 0; i < emptyFrameLines.Length; i++)
+            {
+                // for every totalFrame
+                for (int j = 0; j < frames; j++)
+                {
+                    //print every line of empty frame
+                    Console.Write(emptyFrameLines[i]);
+                    // if at the end of loop
+                    if (j == frames - 1)
+                    {
+                        // print closing part of the empty frame
+                        Console.Write(closingFrameLines[i]);
+                    }
+                }
+                //spacing
+                Console.WriteLine();
+            }
         }
 
     }
