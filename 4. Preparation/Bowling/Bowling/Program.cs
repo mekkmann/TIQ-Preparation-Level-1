3120718dc2020fd21e36f391df4cf3f9ccc9ef52
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-09 23:25:03 +0100

    Bowling Unit1 Mission Completed

--- 4. Preparation/Bowling/Bowling/Program.cs
+++ 4. Preparation/Bowling/Bowling/Program.cs
@@ -9,19 +9,21 @@
             // set totalFrames to a random number between 1-10
             int totalFrames = random.Next(1, 11);
 
-            DrawEmptyFrames(totalFrames);
-            totalFrames = random.Next(1, 11);
-            DrawEmptyFrames(totalFrames);
+            //DrawFrames(totalFrames);
+            //totalFrames = random.Next(1, 11);
+            totalFrames = 5;
+            DrawFrames(totalFrames);
 
             Console.ReadLine();
         }
 
-        static void DrawEmptyFrames(int frames)
+        static void DrawFrames(int frames)
         {
             //path to emptyFrame ascii art
             string pathToEmptyFrame = "EmptyFrame.txt";
             //path to closingFrame ascii art
             string pathToClosingFrame = "FrameClosing.txt";
+
             // read file and store all lines in an array
             string[] emptyFrameLines = File.ReadAllLines(pathToEmptyFrame);
             string[] closingFrameLines = File.ReadAllLines(pathToClosingFrame);
@@ -32,8 +34,33 @@
                 // for every totalFrame
                 for (int j = 0; j < frames; j++)
                 {
-                    //print every line of empty frame
-                    Console.Write(emptyFrameLines[i]);
+                    //if i equals 1 (where we write the score)
+                    if (i == 1)
+                    {
+                        // roll bowling ball and get results oh rolls
+                        int[] rolls = Roll();
+                        // translate results to symbols
+                        string[] rollSymbols = TranslateRollToSymbols(rolls);
+                        // if strike, length will be 1
+                        if (rolls.Length == 1)
+                        {
+                            // print line with symbols
+                            Console.Write($"| |{rollSymbols[0]}| ");
+                        }
+                        // if not a strike, length will be over 1
+                        else
+                        {
+                            // print line with symbols
+                            Console.Write($"| |{rollSymbols[0]}|{rollSymbols[1]}");
+                        }
+                    }
+                    // if we shouldn't write the score
+                    else
+                    {
+                        // print the rest of the frame
+                        Console.Write(emptyFrameLines[i]);
+                    }
+
                     // if at the end of loop
                     if (j == frames - 1)
                     {
@@ -44,6 +71,50 @@
                 //spacing
                 Console.WriteLine();
             }
+        }
+
+        static int[] Roll()
+        {
+            Random random = new Random();
+
+            int totalPins = 10;
+            int firstRoll = random.Next(0, totalPins + 1);
+            if (firstRoll == 10)
+            {
+                return [10];
+            }
+            int secondRoll = random.Next(0, totalPins + 1 - firstRoll);
+
+            return [firstRoll, secondRoll];
+        }
+
+        static string[] TranslateRollToSymbols(int[] roll)
+        {
+            if (roll.Length == 1)
+            {
+                return ["X"];
+            }
+
+            string symbol1 = roll[0].ToString();
+            string symbol2 = roll[1].ToString();
+
+            switch (roll[0])
+            {
+                case 0:
+                    symbol1 = "-";
+                    break;
+            }
+
+            if (roll[1] == 0)
+            {
+                symbol2 = "-";
+            }
+            else if (roll[1] == 10 && roll[0] == 0 || roll[1] + roll[0] == 10)
+            {
+                symbol2 = "/";
+            }
+
+            return [symbol1, symbol2];
         }
 
     }
