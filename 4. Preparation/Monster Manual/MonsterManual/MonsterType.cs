f648479f12029791ea619daa570fcd559ccb9531
Pontus (Alexander) Liljekvist <alex.p.liljekvist@gmail.com>
2024-03-10 15:32:39 +0100

    MonsterManual Unit3 BossLevel Completed

--- 4. Preparation/Monster Manual/MonsterManual/MonsterType.cs
+++ 4. Preparation/Monster Manual/MonsterManual/MonsterType.cs
@@ -1,8 +1,23 @@
 ﻿namespace MonsterManual
 {
+    // armor type enum
+    enum ArmorTypeId
+    {
+        Unspecified,
+        Natural,
+        Leather,
+        StuddedLeather,
+        Hide,
+        ChainShirt,
+        ChainMail,
+        ScaleMail,
+        Plate,
+        Other
+    }
+
     // defining a class
     // (primary constructor)
-    internal class MonsterType(string name, string description, string alignment, string hpRoll)
+    internal class MonsterType(string name, string description, string alignment, string hpRoll, int armorClass, ArmorTypeId armorType = ArmorTypeId.Unspecified)
     {
         //variables
         //name
@@ -13,6 +28,10 @@
         public string alignment = alignment;
         //dice notation for hp roll (or just hp in rare cases)
         public string hpRoll = hpRoll;
+        //armor class
+        public int armorClass = armorClass;
+        //armor type
+        public ArmorTypeId armorType = armorType;
 
         // functions
 
@@ -27,6 +46,10 @@
             Console.WriteLine($"Alignment: {alignment}");
             //prints hp roll
             Console.WriteLine($"Hit points roll: {hpRoll}");
+            //prints armor class
+            Console.WriteLine($"Armor class: {armorClass}");
+            //prints armor type
+            Console.WriteLine($"Armor type: {armorType}");
         }
     }
 }
--- 4. Preparation/Monster Manual/MonsterManual/Program.cs
+++ 4. Preparation/Monster Manual/MonsterManual/Program.cs
@@ -16,28 +16,69 @@ namespace MonsterManual
 
             // prints title
             Console.WriteLine("MONSTER MANUAL\n");
-            // prints instruction
-            Console.WriteLine("Enter a query to search monsters by name:");
+
+            // prints instruction to choose type of query
+            Console.WriteLine("Do you want to search by (n)ame or (a)rmor type?:\n");
+            // store query options
+            ConsoleKeyInfo consoleKeyInput = Console.ReadKey(true);
+
             // stores query results
             List<MonsterType> queryResults = [];
             // stores query input
             string queryInput = string.Empty;
-
             // do at least once and then while there are no results in the query
             do
             {
-                queryInput = Console.ReadLine();
-                foreach (MonsterType monster in allMonsterTypes)
+                // if they chose to search by name
+                if (consoleKeyInput.Key == ConsoleKey.N)
                 {
-                    if (monster.name.Contains(queryInput, StringComparison.CurrentCultureIgnoreCase))
+                    // prints instruction
+                    Console.WriteLine("Enter a query to search monsters by name:");
+                    queryInput = Console.ReadLine();
+                    foreach (MonsterType monster in allMonsterTypes)
                     {
-                        queryResults.Add(monster);
+                        if (monster.name.Contains(queryInput, StringComparison.CurrentCultureIgnoreCase))
+                        {
+                            queryResults.Add(monster);
+                        }
                     }
                 }
+                // if they chose to search by armor type
+                if (consoleKeyInput.Key == ConsoleKey.A)
+                {
+                    // prints instruction
+                    Console.WriteLine("\nWhich armor type do you want to display?\n");
+                    // stores all values in armor type enum
+                    ArmorTypeId[] armorTypeValues = Enum.GetValues<ArmorTypeId>();
+                    for (var i = 0; i < armorTypeValues.Length; i++)
+                    {
+                        Console.WriteLine($"{i + 1}: {armorTypeValues[i]}");
+                    }
+
+                    // armorToSearchByInput 
+                    int armorIndex = int.Parse(Console.ReadLine()) - 1;
+                    // chosen armor type 
+                    ArmorTypeId chosenArmorType = armorTypeValues[armorIndex];
+
+                    // for each monster in list
+                    foreach (MonsterType monster in allMonsterTypes)
+                    {
+                        // if the armor type is the armor type we chose to look for
+                        if (monster.armorType == chosenArmorType)
+                        {
+                            // add monster to query results
+                            queryResults.Add(monster);
+                        }
+
+
+                    }
+                }
+                // if no results, run loop again
                 if (queryResults.Count == 0)
                 {
-                    Console.WriteLine("\nNo monsters found. Try again:");
+                    Console.WriteLine("\nNo monsters found. Try again.");
                 }
+
 
             } while (queryResults.Count == 0);
 
@@ -99,6 +140,8 @@ namespace MonsterManual
             string currentDescription = string.Empty;
             string currentAlignment = string.Empty;
             string currentHpRoll = string.Empty;
+            string currentArmorClass = string.Empty;
+            ArmorTypeId currentArmorType = ArmorTypeId.Unspecified;
 
             // list of monster types
             List<MonsterType> monsterTypes = [];
@@ -157,6 +200,74 @@ namespace MonsterManual
                         currentHpRoll = match.Groups[1].Value;
                     }
                 }
+
+                //armor class and armor type
+                if (monsterLineCounter == 4)
+                {
+                    // pattern for armor class and armor type
+                    string armorPattern = @"(\d+) (\(.*\))?";
+                    //getting matches for the line
+                    Match match = Regex.Match(line, armorPattern);
+                    // check if armor type exists
+                    if (!string.IsNullOrWhiteSpace(match.Groups[2].Value))
+                    {
+                        // get the armor type as string and trim parentheses
+                        string armorTypeString = match.Groups[2].Value.Trim(['(', ')']);
+                        // get the first part of armor type
+                        string[] armorTypeParts = armorTypeString.Split(',');
+
+                        // set armor type
+                        switch (armorTypeParts[0].ToLower())
+                        {
+                            // to natural armor
+                            case ("natural armor"):
+                                currentArmorType = ArmorTypeId.Natural;
+                                break;
+                            // to leather armor
+                            case ("leather"):
+                                currentArmorType = ArmorTypeId.Leather;
+                                break;
+                            // to studded leather
+                            case ("studded leather"):
+                                currentArmorType = ArmorTypeId.StuddedLeather;
+                                break;
+                            // to hide
+                            case ("hide"):
+                                currentArmorType = ArmorTypeId.Hide;
+                                break;
+                            // to chain shirt
+                            case ("chain shirt"):
+                                currentArmorType = ArmorTypeId.ChainShirt;
+                                break;
+                            // to chain mail
+                            case ("chain mail"):
+                                currentArmorType = ArmorTypeId.ChainMail;
+                                break;
+                            // to scale mail
+                            case ("scale mail"):
+                                currentArmorType = ArmorTypeId.ScaleMail;
+                                break;
+                            // to plate
+                            case ("plate"):
+                                currentArmorType = ArmorTypeId.Plate;
+                                break;
+                            // to other if no cases are met
+                            default:
+                                currentArmorType = ArmorTypeId.Other;
+                                break;
+                        }
+                    }
+                    // otherwise set it to unspecified
+                    else
+                    {
+                        currentArmorType = ArmorTypeId.Unspecified;
+                    }
+
+                    // set current armor class to value
+                    currentArmorClass = match.Groups[1].Value;
+
+                }
+
                 //check if at the end of a monster
                 if (line.Length == 0)
                 {
@@ -166,20 +277,24 @@ namespace MonsterManual
                     // set previous line
                     previousLine = line;
 
-                    // create and add monster to list
+                    // create and add monster type to list
+                    // creating the monster with the temp variables from before
                     monsterTypes.Add(
                         new MonsterType(
                             currentName,
                             currentDescription,
                             currentAlignment,
-                            currentHpRoll)
+                            currentHpRoll,
+                            int.Parse(currentArmorClass),
+                            currentArmorType)
                         );
-
                     // reset temp variables
                     currentName = string.Empty;
                     currentDescription = string.Empty;
                     currentAlignment = string.Empty;
                     currentHpRoll = string.Empty;
+                    currentArmorClass = string.Empty;
+                    currentArmorType = ArmorTypeId.Unspecified;
                 }
 
             }
